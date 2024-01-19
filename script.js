4c1f93ef402e9fbc1f4a431eebe57a78da5ab43b
rahultaakhr@gmail.com <rahultaakhr@gmail.com>
2024-01-19 15:06:40 +0530

    personal

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log('hellu');
+const main_Input=document.getElementById('main-input')
+const main_Button=document.getElementById('main-button')
+let AfterClick=document.querySelector('.afterClick')
+
+main_Button.addEventListener('click',async function clickE(){
+    console.log(main_Input.value);
+  
+  AfterClick.innerHTML=`Will You Be My GF ${main_Input.value} Ji`
+   main_Input.value= ''
+  
+   setTimeout(()=>{
+ let con=confirm('Maan Jao Na Yaar 🥺')
+if (!con) {
+    AfterClick.innerHTML=`Thik hai jesi apki marzi 😔😔`
+} else {
+    AfterClick.innerHTML=`🙃🙃🥳🥳` 
+}
+setTimeout(() => {
+    window.location.reload()
+}, 6000);
+   },4000)
+})
